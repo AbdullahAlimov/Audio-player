7d9dfdc60a9b3041162e4337a1398cd6fda6679e
AbdullahAlimov <63803994+AbdullahAlimov@users.noreply.github.com>
2023-05-14 12:00:03 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,21 +1,12 @@
-new Swiper(".content",{ 
-    spaceBetween:150,
-    simulateTouch:true,
-    effect: 'fade',
-    fadeEffect:{
-        crossFade:true,
-    },
-    navigation: {
-        nextEl: ".navigation__next",
-        prevEl: ".navigation__prev",
-    },
-});
-
+initSwiper();
 function refreshSwiper(swiper){
     swiper.destroy();
+    initSwiper();
+}
+function initSwiper(){
     new Swiper(".content",{ 
         spaceBetween:150,
-        simulateTouch:true,
+        allowTouchMove: false,
         effect: 'fade',
         fadeEffect:{
             crossFade:true,
